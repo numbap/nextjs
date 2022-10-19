bb08f82c4f9559989ae5201424fc053bacb543e8
Pat Jo <patjo@Pats-MacBook-Air.local>
2022-10-19 11:32:50 -0400

    ready for first build

---
+++ pages/posts/first-post.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Link from 'next/link';
+import Script from 'next/script';
+import Layout from '../../components/layout';
+
+
+export default function FirstPost() {
+    return (
+        <Layout>
+            <Head>
+                <title>First Post</title>
+                <link rel="icon" href="/favicon.ico" />
+                <Script
+                    src="https://connect.facebook.net/en_US/sdk.js"
+                    strategy="lazyOnload"
+                    onLoad={() =>
+                        console.log(`script loaded correctly, window.FB has been populated`)
+                    }
+                />            </Head>
+
+            <h1>First Post</h1>
+        </Layout>);
+}
